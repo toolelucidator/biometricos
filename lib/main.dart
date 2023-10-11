ccb381863ea273d170f0ebed9fbb98b7e8d5827d
toolelucidator <jorge.tapiaupt@outlook.com>
2023-10-11 08:14:24 -0600

    Fixed Android dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:local_auth/local_auth.dart';
 import 'Calculator.dart';
